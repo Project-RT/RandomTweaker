a4dc40b5f116b84367e517b02277480cb3f6f8ce
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-05-05 23:02:23 +0800

    IMouse

--- src/main/java/com/ikexing/icrtweaker/events/eventHandler.java
+++ src/main/java/com/ikexing/icrtweaker/events/eventHandler.java
@@ -3,10 +3,14 @@
 //import com.ikexing.icrtweaker.ICRTweaker;
 //import hellfirepvp.astralsorcery.common.data.research.PlayerProgress;
 //import hellfirepvp.astralsorcery.common.data.research.ResearchManager;
+//import net.minecraft.client.Minecraft;
+//import net.minecraft.entity.Entity;
+//import net.minecraft.entity.EntityLivingBase;
 //import net.minecraft.entity.player.EntityPlayer;
 //import net.minecraft.init.Items;
 //import net.minecraft.util.EnumHand;
 //import net.minecraft.world.World;
+//import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 //import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 //import net.minecraftforge.fml.common.Mod;
 //import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -15,15 +19,15 @@
 //public class eventHandler {
 //
 //    @SubscribeEvent
-//    public static void onPlayerLeftClickBlockEvent(PlayerInteractEvent.LeftClickBlock event){
+//    public static void onPlayerLeftClickBlockEvent(PlayerInteractEvent.RightClickItem event){
 //        EntityPlayer player = event.getEntityPlayer();
 //        World world = event.getEntityPlayer().getEntityWorld();
 //
 //        if(!world.isRemote && player.getHeldItem(EnumHand.MAIN_HAND).getItem() == Items.STICK){
-//            PlayerProgress prog = ResearchManager.getProgress(player, event.getSide());
-//
-//            System.out.println(prog.getAttunedConstellation().getSimpleName() );
-//            System.out.println(prog.getPerkLevel(player));
+//            System.out.println(Minecraft.getMinecraft().objectMouseOver.getBlockPos().getX());
+//            System.out.println(Minecraft.getMinecraft().objectMouseOver.getBlockPos().getY());
+//            System.out.println(Minecraft.getMinecraft().objectMouseOver.getBlockPos().getZ());
 //        }
 //    }
+//
 //}
---
+++ src/main/java/com/ikexing/icrtweaker/mouse/IMouse.java
@@ -0,0 +1,29 @@
+package com.ikexing.icrtweaker.mouse;
+
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.entity.IEntity;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import crafttweaker.api.world.IBlockPos;
+import crafttweaker.api.world.IVector3d;
+import net.minecraft.client.Minecraft;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+@ZenRegister
+@ZenClass("mods.icrtweaker.IMouse")
+public class IMouse {
+    @ZenMethod
+    public static IVector3d getMouseHit(){
+        return CraftTweakerMC.getIVector3d(Minecraft.getMinecraft().objectMouseOver.hitVec);
+    }
+
+    @ZenMethod
+    public static IBlockPos getMouseHitBlock(){
+        return CraftTweakerMC.getIBlockPos(Minecraft.getMinecraft().objectMouseOver.getBlockPos());
+    }
+
+    @ZenMethod
+    public static IEntity getMouseHitEntity(){
+        return CraftTweakerMC.getIEntity(Minecraft.getMinecraft().objectMouseOver.entityHit);
+    }
+}
