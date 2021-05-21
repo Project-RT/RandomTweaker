2adde4262cdcf4e032ab0b772b048ee5aba2725f
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-05-21 15:38:07 +0800

    thebetweenlands crt support

---
+++ src/main/java/com/ikexing/randomtweaker/api/player/IPlayerExpansionTBL.java
@@ -0,0 +1,92 @@
+package com.ikexing.randomtweaker.api.player;
+
+import crafttweaker.annotations.ModOnly;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import crafttweaker.api.player.IPlayer;
+import net.minecraft.entity.player.EntityPlayer;
+import stanhebben.zenscript.annotations.ZenExpansion;
+import stanhebben.zenscript.annotations.ZenMethod;
+import thebetweenlands.api.capability.IDecayCapability;
+import thebetweenlands.common.registries.CapabilityRegistry;
+
+/**
+ * @author ikexing
+ */
+@ZenRegister
+@ModOnly("thebetweenlands")
+@ZenExpansion("crafttweaker.player.IPlayer")
+public class IPlayerExpansionTBL {
+
+    @ZenMethod
+    public static boolean isCapBeNull(IPlayer player) {
+        EntityPlayer mcPlayer = CraftTweakerMC.getPlayer(player);
+        IDecayCapability cap = mcPlayer.getCapability(CapabilityRegistry.CAPABILITY_DECAY, null);
+        return cap == null;
+    }
+
+    @ZenMethod
+    public static void addStats(IPlayer player, int decay, float saturationModifier) {
+        EntityPlayer mcPlayer = CraftTweakerMC.getPlayer(player);
+        IDecayCapability cap = mcPlayer.getCapability(CapabilityRegistry.CAPABILITY_DECAY, null);
+        assert cap != null;
+        cap.getDecayStats().addStats(decay, saturationModifier);
+    }
+
+    @ZenMethod
+    public static int getDecayLevel(IPlayer player) {
+        EntityPlayer mcPlayer = CraftTweakerMC.getPlayer(player);
+        IDecayCapability cap = mcPlayer.getCapability(CapabilityRegistry.CAPABILITY_DECAY, null);
+        assert cap != null;
+        return cap.getDecayStats().getDecayLevel();
+    }
+
+    @ZenMethod
+    public static int getPrevDecayLevel(IPlayer player) {
+        EntityPlayer mcPlayer = CraftTweakerMC.getPlayer(player);
+        IDecayCapability cap = mcPlayer.getCapability(CapabilityRegistry.CAPABILITY_DECAY, null);
+        assert cap != null;
+        return cap.getDecayStats().getPrevDecayLevel();
+    }
+
+    @ZenMethod
+    public static void addDecayAcceleration(IPlayer player, float acceleration) {
+        EntityPlayer mcPlayer = CraftTweakerMC.getPlayer(player);
+        IDecayCapability cap = mcPlayer.getCapability(CapabilityRegistry.CAPABILITY_DECAY, null);
+        assert cap != null;
+        cap.getDecayStats().addDecayAcceleration(acceleration);
+    }
+
+    @ZenMethod
+    public static float getSaturationLevel(IPlayer player) {
+        EntityPlayer mcPlayer = CraftTweakerMC.getPlayer(player);
+        IDecayCapability cap = mcPlayer.getCapability(CapabilityRegistry.CAPABILITY_DECAY, null);
+        assert cap != null;
+        return cap.getDecayStats().getSaturationLevel();
+    }
+
+    @ZenMethod
+    public static float getAccelerationLevel(IPlayer player) {
+        EntityPlayer mcPlayer = CraftTweakerMC.getPlayer(player);
+        IDecayCapability cap = mcPlayer.getCapability(CapabilityRegistry.CAPABILITY_DECAY, null);
+        assert cap != null;
+        return cap.getDecayStats().getAccelerationLevel();
+    }
+
+    @ZenMethod
+    public static void setDecayLevel(IPlayer player, int decay) {
+        EntityPlayer mcPlayer = CraftTweakerMC.getPlayer(player);
+        IDecayCapability cap = mcPlayer.getCapability(CapabilityRegistry.CAPABILITY_DECAY, null);
+        assert cap != null;
+        cap.getDecayStats().setDecayLevel(decay);
+    }
+
+    @ZenMethod
+    public static void setDecaySaturationLevel(IPlayer player, float saturation) {
+        EntityPlayer mcPlayer = CraftTweakerMC.getPlayer(player);
+        IDecayCapability cap = mcPlayer.getCapability(CapabilityRegistry.CAPABILITY_DECAY, null);
+        assert cap != null;
+        cap.getDecayStats().setDecaySaturationLevel(saturation);
+    }
+
+}
---
+++ src/main/java/com/ikexing/randomtweaker/api/tbl/TBLItemHandler.java
@@ -0,0 +1,66 @@
+package com.ikexing.randomtweaker.api.tbl;
+
+import crafttweaker.annotations.ModOnly;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.block.IBlockState;
+import crafttweaker.api.item.IItemDefinition;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+import thebetweenlands.api.item.CorrosionHelper;
+
+import java.util.List;
+
+/**
+ * @author ikexing
+ */
+@ZenRegister
+@ModOnly("thebetweenlands")
+@ZenClass("mods.randomtweaker.ItemTbl")
+public class TBLItemHandler {
+    @ZenMethod
+    public static void addCorrosionPropertyOverrides(IItemDefinition item) {
+        CorrosionHelper.addCorrosionPropertyOverrides(CraftTweakerMC.getItem(item));
+    }
+
+    @ZenMethod
+    public static void getModifier(IItemStack item) {
+        CorrosionHelper.getModifier(CraftTweakerMC.getItemStack(item));
+    }
+
+    @ZenMethod
+    public static void getDestroySpeed(IItemStack item, float normalStrength, IBlockState blockState) {
+        CorrosionHelper.getDestroySpeed(normalStrength, CraftTweakerMC.getItemStack(item), CraftTweakerMC.getBlockState(blockState));
+    }
+
+    @ZenMethod
+    public static boolean shouldCauseBlockBreakReset(IItemStack oldStack, IItemStack newStack) {
+        return CorrosionHelper.shouldCauseBlockBreakReset(CraftTweakerMC.getItemStack(oldStack), CraftTweakerMC.getItemStack(newStack));
+    }
+
+    @ZenMethod
+    public static boolean shouldCauseReequipAnimation(IItemStack oldStack, IItemStack newStack, boolean slotChanged) {
+        return CorrosionHelper.shouldCauseReequipAnimation(CraftTweakerMC.getItemStack(oldStack), CraftTweakerMC.getItemStack(newStack), slotChanged);
+    }
+
+    @ZenMethod
+    public static boolean areItemStackTagsEqual(IItemStack oldStack, IItemStack newStack) {
+        return CorrosionHelper.areItemStackTagsEqual(CraftTweakerMC.getItemStack(oldStack), CraftTweakerMC.getItemStack(newStack));
+    }
+
+    @ZenMethod
+    public static void addCorrosionTooltips(IItemStack stack, List<String> lines, boolean advancedItemTooltips) {
+        CorrosionHelper.addCorrosionTooltips(CraftTweakerMC.getItemStack(stack), lines, advancedItemTooltips);
+    }
+
+    @ZenMethod
+    public static int getCoatingStage(IItemStack stack) {
+        return CorrosionHelper.getCoatingStage(CraftTweakerMC.getItemStack(stack));
+    }
+
+    @ZenMethod
+    public static int getCorrosionStage(IItemStack stack) {
+        return CorrosionHelper.getCorrosionStage(CraftTweakerMC.getItemStack(stack));
+    }
+}
