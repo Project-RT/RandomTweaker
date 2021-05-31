9339dff054b7502f07d730e09132dd1bdf0ebb0c
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-05-31 23:00:25 +0800

    Update RTPlugin.java

--- src/main/java/com/ikexing/randomtweaker/impl/core/RTPlugin.java
+++ src/main/java/com/ikexing/randomtweaker/impl/core/RTPlugin.java
@@ -1,47 +1,49 @@
 package com.ikexing.randomtweaker.impl.core;
 
+import net.minecraftforge.fml.relauncher.CoreModManager;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
 import javax.annotation.Nullable;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.CodeSource;
 import java.util.Map;
 
 /**
  * @author ikexing
  */
-@IFMLLoadingPlugin.Name("randomtweaker")
-@IFMLLoadingPlugin.SortingIndex(-7500)
+@IFMLLoadingPlugin.Name("randomtweakerCore")
 @IFMLLoadingPlugin.MCVersion("1.12.2")
 public class RTPlugin implements IFMLLoadingPlugin {
     public RTPlugin() {
         MixinBootstrap.init();
-        Mixins.addConfiguration("mixins.randomtweaker.core.json");
         Mixins.addConfiguration("mixins.randomtweaker.init.json");
+        CodeSource codeSource = this.getClass().getProtectionDomain().getCodeSource();
+        if (codeSource != null) {
+            URL location = codeSource.getLocation();
+            try {
+                File file = new File(location.toURI());
+                if (file.isFile()) {
+                    CoreModManager.getReparseableCoremods().remove(file.getName());
+                }
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        } else {
+            LogManager.getLogger().warn("No CodeSource, if this is not a development environment we might run into problems!");
+            LogManager.getLogger().warn(this.getClass().getProtectionDomain());
+        }
     }
 
-    @Override
-    public String[] getASMTransformerClass() {
+    @Override public String[] getASMTransformerClass() {
         return new String[0];
     }
-
-    @Override
-    public String getModContainerClass() {
-        return null;
-    }
-
-    @Nullable
-    @Override
-    public String getSetupClass() {
-        return null;
-    }
-
-    @Override
-    public void injectData(Map<String, Object> data) {
-    }
-
-    @Override
-    public String getAccessTransformerClass() {
-        return null;
-    }
+    @Override public String getModContainerClass() { return null; }
+    @Nullable @Override public String getSetupClass() { return null; }
+    @Override public void injectData(Map<String, Object> data) {}
+    @Override public String getAccessTransformerClass() { return null; }
 }
