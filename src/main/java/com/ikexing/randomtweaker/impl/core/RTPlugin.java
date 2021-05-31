91a363db57e2d648a1e2a53c8d05f70c8c5aa877
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-05-31 22:33:42 +0800

    Update RTPlugin.java

--- src/main/java/com/ikexing/randomtweaker/impl/core/RTPlugin.java
+++ src/main/java/com/ikexing/randomtweaker/impl/core/RTPlugin.java
@@ -21,8 +21,8 @@ import java.util.Map;
 public class RTPlugin implements IFMLLoadingPlugin {
     public RTPlugin() {
         MixinBootstrap.init();
-        Mixins.addConfiguration("mixins.randomtweaker.init.json");
         Mixins.addConfiguration("mixins.randomtweaker.core.json");
+        Mixins.addConfiguration("mixins.randomtweaker.init.json");
         CodeSource codeSource = this.getClass().getProtectionDomain().getCodeSource();
         if (codeSource != null) {
             URL location = codeSource.getLocation();
