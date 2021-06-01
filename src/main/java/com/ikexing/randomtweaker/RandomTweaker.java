842e619293be3d843f652613c8e8398f3c4ffb9a
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-06-01 12:11:51 +0800

    required JEID

--- src/main/java/com/ikexing/randomtweaker/RandomTweaker.java
+++ src/main/java/com/ikexing/randomtweaker/RandomTweaker.java
@@ -40,7 +40,7 @@ public class RandomTweaker {
     public static final String MODID = "randomtweaker";
     public static final String NAME = "RandomTweaker";
     public static final String VERSION = "1.0.0";
-    public static final String DESPENDENCIES = "required-after:crafttweaker";
+    public static final String DESPENDENCIES = "required-after:crafttweaker;required:jeid";
 
     public static final String THAUMCRAFT = "thaumcraft";
 
