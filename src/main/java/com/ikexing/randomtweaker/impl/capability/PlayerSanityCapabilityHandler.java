164b3cefacc0b4efc32873ced05ad56c5c7800e8
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-06-12 15:56:52 +0800

    sanity network

--- src/main/java/com/ikexing/randomtweaker/impl/capability/PlayerSanityCapabilityHandler.java
+++ src/main/java/com/ikexing/randomtweaker/impl/capability/PlayerSanityCapabilityHandler.java
@@ -1,6 +1,8 @@
 package com.ikexing.randomtweaker.impl.capability;
 
 import com.ikexing.randomtweaker.RandomTweaker;
+import com.ikexing.randomtweaker.impl.network.PlayerSanityNetWork;
+import com.ikexing.randomtweaker.impl.utils.cap.PlayerSanityHelper;
 import java.util.Objects;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
@@ -8,11 +10,13 @@ import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.Capability.IStorage;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -58,5 +62,22 @@ public class PlayerSanityCapabilityHandler {
             .getCapability(PlayerSanityCapabilityHandler.PlayerSanityCap, null);
 
         Objects.requireNonNull(instance).setSanity(Objects.requireNonNull(original).getSanity());
+    }
+
+    @SubscribeEvent
+    public static void onPlayerJoinWorld(EntityJoinWorldEvent event) {
+        Entity entity = event.getEntity();
+        if (!event.getWorld().isRemote && entity instanceof EntityPlayer) {
+            PlayerSanityCapability sanityCap = PlayerSanityHelper
+                .getPlayerSanity((EntityPlayer) entity);
+            if (sanityCap.getOriginalSanity() == 0) {
+                double random = Math.random();
+                sanityCap.setOriginalSanity(
+                    (int) (random >= 0.6 ? random * 100 : (random + (random / 2)) * 100));
+            }
+            PlayerSanityNetWork.Sanity.sendClientCustomPacket((EntityPlayer) entity);
+            entity.sendMessage(new TextComponentString(
+                sanityCap.getOriginalSanity() + " " + sanityCap.getSanity()));
+        }
     }
 }
---
+++ src/main/java/com/ikexing/randomtweaker/impl/network/PlayerSanityNetWork.java
@@ -0,0 +1,58 @@
+package com.ikexing.randomtweaker.impl.network;
+
+import com.ikexing.randomtweaker.impl.capability.PlayerSanityCapability;
+import com.ikexing.randomtweaker.impl.utils.cap.PlayerSanityHelper;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.FMLEventChannel;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.internal.FMLProxyPacket;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class PlayerSanityNetWork {
+
+    public static void register() {
+        Sanity.CHANNEL.register(Sanity.class);
+    }
+
+    public static class Sanity {
+
+        private static final String NAME = "PLAYERSANITY";
+        private static final FMLEventChannel CHANNEL = NetworkRegistry.INSTANCE
+            .newEventDrivenChannel(NAME);
+
+        public static void sendClientCustomPacket(EntityPlayer player) {
+            PacketBuffer buffer = new PacketBuffer(Unpooled.buffer());
+            PlayerSanityCapability sanityCap = PlayerSanityHelper.getPlayerSanity(player);
+
+            buffer.writeFloat(sanityCap.getSanity());
+            buffer.writeInt(sanityCap.getOriginalSanity());
+
+            CHANNEL.sendTo(new FMLProxyPacket(buffer, NAME), (EntityPlayerMP) player);
+        }
+
+        @SubscribeEvent
+        @SideOnly(Side.CLIENT)
+        public static void onClientCustomPacket(FMLNetworkEvent.ClientCustomPacketEvent event) {
+            ByteBuf buffer = event.getPacket().payload();
+            int oSanity = buffer.readInt();
+            float sanity = buffer.readFloat();
+            Minecraft mc = Minecraft.getMinecraft();
+
+            mc.addScheduledTask(() -> {
+                EntityPlayer player = mc.player;
+                PlayerSanityCapability sanityCap = PlayerSanityHelper.getPlayerSanity(player);
+
+                sanityCap.setSanity(sanity);
+                sanityCap.setOriginalSanity(oSanity);
+            });
+        }
+    }
+}
