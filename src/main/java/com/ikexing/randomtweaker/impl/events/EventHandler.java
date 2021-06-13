18b5dd5905c91e17f1363dd9b016d41e7464d413
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-06-13 14:53:03 +0800

    config and joinworld

--- src/main/java/com/ikexing/randomtweaker/impl/events/EventHandler.java
+++ src/main/java/com/ikexing/randomtweaker/impl/events/EventHandler.java
@@ -1,15 +1,23 @@
 package com.ikexing.randomtweaker.impl.events;
 
 import com.ikexing.randomtweaker.RandomTweaker;
+import com.ikexing.randomtweaker.impl.client.capability.PlayerSanityCapability;
+import com.ikexing.randomtweaker.impl.client.utils.cap.PlayerSanityHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionType;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.registries.IForgeRegistry;
 
 @EventBusSubscriber
-public class Registry {
+public class EventHandler {
 
     @SubscribeEvent
     public static void onPotionRegistry(Register<Potion> event) {
@@ -24,6 +32,28 @@ public class Registry {
         IForgeRegistry<PotionType> registry = event.getRegistry();
         if (!RandomTweaker.potionTypeList.isEmpty()) {
             RandomTweaker.potionTypeList.forEach((k, v) -> registry.register(v));
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerJoinWorld(EntityJoinWorldEvent event) {
+        Entity entity = event.getEntity();
+        if (!event.getWorld().isRemote && entity instanceof EntityPlayer) {
+            PlayerSanityCapability sanityCap = PlayerSanityHelper
+                .getPlayerSanity((EntityPlayer) entity);
+            if (sanityCap.getOriginalSanity() == 0) {
+                double random = Math.random();
+                sanityCap.setOriginalSanity(
+                    (int) (random >= 0.6 ? random * 100 : (random + (random / 2)) * 100));
+            }
+            PlayerSanityHelper.sync(entity);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+        if (event.getModID().equals(RandomTweaker.MODID)) {
+            ConfigManager.sync(RandomTweaker.MODID, Config.Type.INSTANCE);
         }
     }
 }
