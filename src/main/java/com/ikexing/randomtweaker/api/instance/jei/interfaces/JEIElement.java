6c54936f2da6cd0ce256eabc20790d896d2f0d9f
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-06-17 12:14:12 +0800

    Create JEIElement.java

---
+++ src/main/java/com/ikexing/randomtweaker/api/instance/jei/interfaces/JEIElement.java
@@ -0,0 +1,10 @@
+package com.ikexing.randomtweaker.api.instance.jei.interfaces;
+
+import crafttweaker.annotations.ZenRegister;
+import stanhebben.zenscript.annotations.ZenClass;
+
+@ZenRegister
+@ZenClass("mods.randomtweaker.JEIElement")
+public interface JEIElement {
+
+}
