1b156df438c8275006e328f86c896be22d7540f6
superhelo <3181063382@qq.com>
2021-07-16 18:11:57 +0800

    Fix Bug

--- src/main/java/ink/ikx/rt/api/mods/cote/function/botania/CanSelect.java
+++ src/main/java/ink/ikx/rt/api/mods/cote/function/botania/CanSelect.java
@@ -1,7 +1,7 @@
 package ink.ikx.rt.api.mods.cote.function.botania;
 
 import com.teamacronymcoders.contenttweaker.api.ctobjects.blockpos.IBlockPos;
-import com.teamacronymcoders.contenttweaker.api.ctobjects.entity.player.CTPlayer;
+import com.teamacronymcoders.contenttweaker.api.ctobjects.entity.player.ICTPlayer;
 import crafttweaker.api.item.IItemStack;
 import crafttweaker.api.world.IFacing;
 import ink.ikx.rt.impl.utils.annotation.RTRegisterClass;
@@ -12,5 +12,5 @@ import stanhebben.zenscript.annotations.ZenClass;
 @ZenClass("mods.randomtweaker.cote.CanSelect")
 public interface CanSelect {
 
-    boolean call(CTPlayer player, IItemStack wand, IBlockPos pos, IFacing side);
+    boolean call(ICTPlayer player, IItemStack wand, IBlockPos pos, IFacing side);
 }
