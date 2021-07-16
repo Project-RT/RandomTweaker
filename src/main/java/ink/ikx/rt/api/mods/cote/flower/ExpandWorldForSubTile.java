c5e6f196b9e04ec6e6d21660577f8cd0841ff3e8
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-07-16 19:18:35 +0800

    Create ExpandWorldForSubTile.java

---
+++ src/main/java/ink/ikx/rt/api/mods/cote/flower/ExpandWorldForSubTile.java
@@ -0,0 +1,28 @@
+package ink.ikx.rt.api.mods.cote.flower;
+
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import crafttweaker.api.world.IBlockPos;
+import crafttweaker.api.world.IWorld;
+import ink.ikx.rt.impl.utils.annotation.RTRegisterClass;
+import net.minecraft.tileentity.TileEntity;
+import stanhebben.zenscript.annotations.ZenExpansion;
+import stanhebben.zenscript.annotations.ZenMethod;
+import vazkii.botania.api.subtile.ISubTileContainer;
+import vazkii.botania.api.subtile.SubTileEntity;
+
+@RTRegisterClass({"contenttweaker", "botania"})
+@ZenExpansion("crafttweaker.world.IWorld")
+public class ExpandWorldForSubTile {
+
+    @ZenMethod
+    public static SubTileEntityInGame getSubTileEntityInGame(IWorld world, IBlockPos pos) {
+        TileEntity te = CraftTweakerMC.getWorld(world).getTileEntity(CraftTweakerMC.getBlockPos(pos));
+        if (te instanceof ISubTileContainer) {
+            ISubTileContainer container = (ISubTileContainer) te;
+            SubTileEntity subtile = container.getSubTile();
+            return (SubTileEntityInGame) subtile;
+        }
+        return null;
+    }
+
+}
