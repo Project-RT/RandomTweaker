e0f23ad6738cd37aeca7d3163f56d0b3bd254726
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-07-24 16:19:17 +0800

    Move ModMixin to a new mod Folder

--- src/main/java/ink/ikx/rt/impl/mixins/mods/matteroverdrive/MixinThirstHandler.java
+++ src/main/java/ink/ikx/rt/impl/mixins/mods/matteroverdrive/MixinThirstHandler.java
@@ -1,30 +1,31 @@
-package ink.ikx.rt.impl.mixins.mods;
+package ink.ikx.rt.impl.mixins.mods.matteroverdrive;
 
 import ink.ikx.rt.impl.config.RTConfig;
 import ink.ikx.rt.impl.matteroverdrive.IMatterOverdriveAndroid;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import toughasnails.handler.thirst.ThirstOverlayHandler;
+import toughasnails.thirst.ThirstHandler;
 
 /**
  * @author skyraah
  */
 @Pseudo
-@Mixin(value = ThirstOverlayHandler.class, remap = false)
-public class  MixinThirstOverlayHandler {
+@Mixin(value = ThirstHandler.class, remap = false)
+public class MixinThirstHandler {
 
-    @Inject(method = "onPreRenderOverlay", at = @At(value = "HEAD"), cancellable = true)
-    public void injectOnPreRenderOverlay(RenderGameOverlayEvent.Pre event, CallbackInfo ci) {
-        if (IMatterOverdriveAndroid.isPlayerAndroid(Minecraft.getMinecraft().player)) {
+    @Inject(method = "update", at = @At(value = "HEAD"), cancellable = true)
+    public void injectUpdate(EntityPlayer player, World world, TickEvent.Phase phase, CallbackInfo ci) {
+        if (IMatterOverdriveAndroid.isPlayerAndroid(player)) {
             if (RTConfig.ToughAsNails.AndroidThirst && !RTConfig.ToughAsNails.SelectedStatsThirst) {
                 ci.cancel();
             } else if (RTConfig.ToughAsNails.SelectedStatsThirst && !RTConfig.ToughAsNails.AndroidThirst) {
-                if (IMatterOverdriveAndroid.isUnlocked(Minecraft.getMinecraft().player, RTConfig.ToughAsNails.SelectedStatThirst, 1)) {
+                if (IMatterOverdriveAndroid.isUnlocked(player, RTConfig.ToughAsNails.SelectedStatThirst, 1)) {
                     ci.cancel();
                 }
             }
