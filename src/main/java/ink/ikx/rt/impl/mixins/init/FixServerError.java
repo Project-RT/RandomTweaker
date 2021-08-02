1db2f7e43ed1132a2f2ee1a206f7ca85b4f22d82
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-08-02 13:51:06 +0800

    fix mixin

--- src/main/java/ink/ikx/rt/impl/mixins/init/FixServerError.java
+++ src/main/java/ink/ikx/rt/impl/mixins/init/FixServerError.java
@@ -6,26 +6,19 @@ import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.discovery.ModDiscoverer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * @author friendlyHj <br /> https://github.com/ProjectHDS/HerodotusUtils/blob/main/src/main/java/youyihj/herodotusutils/mixins/init/MixinLoader.java
  */
 @Mixin(value = Loader.class, remap = false, priority = 800)
-public class MixinLoader {
+public class FixServerError {
 
     @Shadow
     private List<ModContainer> mods;
-
-    @Inject(method = "loadMods", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/fml/common/LoadController;transition(Lnet/minecraftforge/fml/common/LoaderState;Z)V", ordinal = 1), remap = false)
-    private void initMixins(List<String> injectedModContainers, CallbackInfo ci) {
-        Mixins.addConfiguration("mixins.randomtweaker.mods.json");
-    }
 
     @Inject(method = "identifyMods", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/fml/common/Loader;identifyDuplicates(Ljava/util/List;)V"), remap = false)
     private void InjectIdentifyMods(List<String> additionalContainers, CallbackInfoReturnable<ModDiscoverer> cir) {
