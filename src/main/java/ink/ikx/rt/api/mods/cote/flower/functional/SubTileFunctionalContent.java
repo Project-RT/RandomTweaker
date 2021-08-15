1ae700ca109da34a1ba2f95799f21e9c003abdbb
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-08-15 16:39:13 +0800

    back to the source

--- src/main/java/ink/ikx/rt/api/mods/cote/flower/functional/SubTileFunctionalContent.java
+++ src/main/java/ink/ikx/rt/api/mods/cote/flower/functional/SubTileFunctionalContent.java
@@ -1,11 +1,18 @@
 package ink.ikx.rt.api.mods.cote.flower.functional;
 
-import crafttweaker.api.data.IData;
+import com.teamacronymcoders.contenttweaker.api.ctobjects.entity.EntityHelper;
+import com.teamacronymcoders.contenttweaker.api.ctobjects.entity.player.CTPlayer;
+import com.teamacronymcoders.contenttweaker.api.ctobjects.enums.Hand;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import ink.ikx.rt.RandomTweaker;
 import ink.ikx.rt.api.mods.cote.flower.SubTileEntityInGame;
+import ink.ikx.rt.api.mods.cote.flower.SubTileRepresentation;
+import java.util.Objects;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
@@ -13,45 +20,83 @@ import net.minecraft.world.World;
 import vazkii.botania.api.subtile.RadiusDescriptor;
 import vazkii.botania.api.subtile.SubTileFunctional;
 
-public abstract class SubTileFunctionalContent extends SubTileFunctional implements SubTileEntityInGame {
+public class SubTileFunctionalContent extends SubTileFunctional implements SubTileEntityInGame {
+
+    public final SubTileRepresentation subtile = RandomTweaker.subTileGeneratingMap.get("aura_flower_f").getValue();
+    public String name;
 
     @Override
-    public abstract RadiusDescriptor getRadius();
+    public RadiusDescriptor getRadius() {
+        return new RadiusDescriptor.Square(toBlockPos(), subtile.getRange());
+    }
 
     @Override
     public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase entity, ItemStack stack) {
         super.onBlockPlacedBy(world, pos, state, entity, stack);
+        if (Objects.nonNull(subtile.onBlockPlaceBy)) {
+            subtile.onBlockPlaceBy.call(CraftTweakerMC.getIWorld(world), CraftTweakerMC.getIBlockPos(pos), CraftTweakerMC.getBlockState(state), EntityHelper.getIEntityLivingBase(entity), CraftTweakerMC.getIItemStack(stack));
+        }
     }
 
     @Override
-    public abstract boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ);
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
+        return !Objects.nonNull(subtile.onBlockActivated) || subtile.onBlockActivated.call(CraftTweakerMC.getIWorld(world), CraftTweakerMC.getIBlockPos(pos), CraftTweakerMC.getBlockState(state), new CTPlayer(player), Hand.of(hand), CraftTweakerMC.getIFacing(side), hitX, hitY, hitZ);
+    }
 
     @Override
-    public abstract void onBlockAdded(World world, BlockPos pos, IBlockState state);
+    public void onBlockAdded(World world, BlockPos pos, IBlockState state) {
+        if (Objects.nonNull(subtile.onBlockAdded)) {
+            subtile.onBlockAdded.call(CraftTweakerMC.getIWorld(world), CraftTweakerMC.getIBlockPos(pos), CraftTweakerMC.getBlockState(state));
+        }
+    }
 
     @Override
-    public abstract void onBlockHarvested(World world, BlockPos pos, IBlockState state, EntityPlayer player);
+    public void onBlockHarvested(World world, BlockPos pos, IBlockState state, EntityPlayer player) {
+        if (Objects.nonNull(subtile.onBlockHarvested)) {
+            subtile.onBlockHarvested.call(CraftTweakerMC.getIWorld(world), CraftTweakerMC.getIBlockPos(pos), CraftTweakerMC.getBlockState(state), new CTPlayer(player));
+        }
+    }
 
     @Override
-    public abstract boolean isOvergrowthAffected();
+    public boolean isOvergrowthAffected() {
+        return subtile.isOvergrowthAffected();
+    }
 
     @Override
-    public abstract int getMaxMana();
+    public int getMaxMana() {
+        return subtile.getMaxMana();
+    }
 
     @Override
-    public abstract int getColor();
+    public int getColor() {
+        return subtile.getColor();
+    }
 
     @Override
-    public abstract boolean canSelect(EntityPlayer player, ItemStack wand, BlockPos pos, EnumFacing side);
+    public boolean canSelect(EntityPlayer player, ItemStack wand, BlockPos pos, EnumFacing side) {
+        if (Objects.nonNull(subtile.canSelect)) {
+            return subtile.canSelect.call(new CTPlayer(player), CraftTweakerMC.getIItemStack(wand), CraftTweakerMC.getIBlockPos(pos), CraftTweakerMC.getIFacing(side));
+        }
+        return true;
+    }
 
     @Override
-    public abstract void updateCustomData(IData data);
+    public void readFromPacketNBT(NBTTagCompound compound) {
+        customData.readFromNBT(compound.getCompoundTag(TAG_CUSTOM_DATA));
+        this.name = compound.getString(TAG_NAME);
+        super.readFromPacketNBT(compound);
+    }
 
     @Override
-    public abstract IData getCustomData();
+    public void writeToPacketNBT(NBTTagCompound compound) {
+        if (!compound.hasKey(TAG_CUSTOM_DATA)) {
+            compound.setTag(TAG_CUSTOM_DATA, new NBTTagCompound());
+        }
+        customData.writeToNBT(compound.getCompoundTag(TAG_NAME));
+        customData.writeToNBT(compound.getCompoundTag(TAG_CUSTOM_DATA));
+        super.writeToPacketNBT(compound);
+    }
 
-    @Override
-    public abstract void setCustomData(IData data);
 
     @Override
     public void addMana(int mana) {
@@ -106,5 +151,14 @@ public abstract class SubTileFunctionalContent extends SubTileFunctional impleme
     @Override
     public String typeOf() {
         return "functional";
+    }
+
+    public static class Mini extends SubTileFunctionalContent {
+
+        @Override
+        public RadiusDescriptor getRadius() {
+            return new RadiusDescriptor.Square(toBlockPos(), ((SubTileFunctionalRepresentation) subtile).getMiniRange());
+        }
+
     }
 }
--- src/main/java/ink/ikx/rt/impl/events/RegEventHandler.java
+++ src/main/java/ink/ikx/rt/impl/events/RegEventHandler.java
@@ -1,11 +1,9 @@
 package ink.ikx.rt.impl.events;
 
-import cn.hutool.core.compiler.CompilerUtil;
 import cn.hutool.core.io.FileUtil;
 import ink.ikx.rt.RandomTweaker;
 import ink.ikx.rt.api.internal.file.Prop;
 import ink.ikx.rt.api.mods.cote.flower.JAVATextContent;
-import ink.ikx.rt.api.mods.cote.flower.functional.SubTileFunctionalRepresentation;
 import ink.ikx.rt.impl.config.RTConfig;
 import java.io.File;
 import java.util.Objects;
@@ -22,11 +20,7 @@ import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.registries.IForgeRegistry;
-import vazkii.botania.api.BotaniaAPI;
-import vazkii.botania.api.BotaniaAPIClient;
-import vazkii.botania.api.subtile.SubTileEntity;
 
-@SuppressWarnings("unchecked")
 @EventBusSubscriber
 public class RegEventHandler {
 
@@ -40,66 +34,16 @@ public class RegEventHandler {
 
     @SubscribeEvent
     public static void onBlockRegistry(Register<Block> event) {
-        if (RandomTweaker.subTileGeneratingMap.isEmpty()) {
-            return;
-        }
-        RandomTweaker.subTileGeneratingMap.forEach((k, v) -> {
-            String className;
-            ClassLoader classLoader;
-            if (v.getKey().equals("generating")) {
-                String Generating = JAVATextContent.GENERATING.replace("${name}", k);
-                className = "ink.ikx.rt.api.mods.cote.flower.generating.CustomSubTileGeneratingContent_" + k;
-                classLoader = CompilerUtil.getCompiler(null)
-                    .addSource(className, Generating)
-                    .compile();
-            } else {
-                String Functional = JAVATextContent.FUNCTIONAL.replace("${name}", k);
-                className = "ink.ikx.rt.api.mods.cote.flower.functional.SubTileFunctionalContentContent_" + k;
-                classLoader = CompilerUtil.getCompiler(null)
-                    .addSource(className, Functional)
-                    .compile();
-            }
 
-            try {
-                Class<? extends SubTileEntity> clazz = (Class<? extends SubTileEntity>) classLoader.loadClass(className);
-                BotaniaAPI.registerSubTile(k, clazz);
-                BotaniaAPI.addSubTileToCreativeMenu(k);
-                for (Class innerClazz : clazz.getDeclaredClasses()) {
-                    if (innerClazz.getSimpleName().equals("Mini")) {
-                        SubTileFunctionalRepresentation v1 = (SubTileFunctionalRepresentation) v.getValue();
-                        if (v1.isHasMini()) {
-                            BotaniaAPI.registerMiniSubTile(k + "Chibi", innerClazz, k);
-                        }
-                    }
-                }
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-        });
     }
 
     @SubscribeEvent
     public static void onModelRegistry(ModelRegistryEvent event) {
         if (RTConfig.RandomTweaker.PlayerSanity) {
             ModelLoader.setCustomModelResourceLocation(RandomTweaker.SANITY_GEM, 0,
-                new ModelResourceLocation(
-                    Objects.requireNonNull(RandomTweaker.SANITY_GEM.getRegistryName()),
-                    "inventory"));
+                new ModelResourceLocation(Objects.requireNonNull(RandomTweaker.SANITY_GEM.getRegistryName()), "inventory"));
         }
-        if (RandomTweaker.subTileGeneratingMap.isEmpty()) {
-            return;
-        }
-        RandomTweaker.subTileGeneratingMap.forEach((k, v) -> {
-            BotaniaAPIClient.registerSubtileModel(k, new ModelResourceLocation("contenttweaker:" + k));
-            createFlowerBlockState(k);
-            if (v.getValue() instanceof SubTileFunctionalRepresentation) {
-                SubTileFunctionalRepresentation v1 = (SubTileFunctionalRepresentation) v.getValue();
-                if (v1.isHasMini()) {
-                    BotaniaAPIClient.registerSubtileModel(k + "Chibi", new ModelResourceLocation("contenttweaker:" + k + "Chibi"));
-                    createFlowerBlockState(k + "Chibi");
-                }
-            }
-        });
+
     }
 
     @SubscribeEvent
