a8c1abb862747ce42c0c0779fd69a847cb137745
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-08-21 23:26:08 +0800

    1.1.7

--- src/main/java/ink/ikx/rt/RandomTweaker.java
+++ src/main/java/ink/ikx/rt/RandomTweaker.java
@@ -69,7 +69,7 @@ public class RandomTweaker {
 
     public static final String MODID = "randomtweaker";
     public static final String NAME = "RandomTweaker";
-    public static final String VERSION = "1.1.4";
+    public static final String VERSION = "1.1.7";
     public static final String GUI_FACTORY = "ink.ikx.rt.impl.config.RTConfigGuiFactory";
     public static final String DESPENDENCIES = "required-after:crafttweaker;after:contenttweaker";
 
