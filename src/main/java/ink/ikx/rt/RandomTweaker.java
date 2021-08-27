1a17583d34647fbaaf8e28c7377341d1a0870918
superhelo <3181063382@qq.com>
2021-08-27 21:56:06 +0800

    1.1.9

--- src/main/java/ink/ikx/rt/RandomTweaker.java
+++ src/main/java/ink/ikx/rt/RandomTweaker.java
@@ -64,7 +64,7 @@ public class RandomTweaker {
 
     public static final String MODID = "randomtweaker";
     public static final String NAME = "RandomTweaker";
-    public static final String VERSION = "1.1.8";
+    public static final String VERSION = "1.1.9";
     public static final String GUI_FACTORY = "ink.ikx.rt.impl.config.RTConfigGuiFactory";
     public static final String DESPENDENCIES = "required-after:crafttweaker;after:contenttweaker";
 
