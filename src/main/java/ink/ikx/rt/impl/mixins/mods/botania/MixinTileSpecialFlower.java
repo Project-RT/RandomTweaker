654bf50904f15a3d5615ee6dafd60be4a6158127
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-08-28 17:41:20 +0800

    Close #33

--- src/main/java/ink/ikx/rt/impl/mixins/mods/botania/MixinTileSpecialFlower.java
+++ src/main/java/ink/ikx/rt/impl/mixins/mods/botania/MixinTileSpecialFlower.java
@@ -3,24 +3,28 @@ package ink.ikx.rt.impl.mixins.mods.botania;
 import cn.hutool.core.lang.Pair;
 import ink.ikx.rt.RandomTweaker;
 import ink.ikx.rt.api.mods.cote.flower.SubTileEntityInGame;
-import java.util.Map.Entry;
 import net.minecraft.nbt.NBTTagCompound;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.api.subtile.SubTileEntity;
+import vazkii.botania.common.block.tile.TileMod;
 import vazkii.botania.common.block.tile.TileSpecialFlower;
+
+import java.util.Map.Entry;
 
 @Pseudo
 @Mixin(value = TileSpecialFlower.class, remap = false)
-public abstract class MixinTileSpecialFlower {
+public abstract class MixinTileSpecialFlower extends TileMod {
 
+    @Final
+    @Shadow
+    private static String TAG_SUBTILE_CMP;
     @Final
     @Shadow
     private static String TAG_SUBTILE_NAME;
@@ -39,15 +43,19 @@ public abstract class MixinTileSpecialFlower {
     protected void injectProvideSubTile(String name, CallbackInfo ci) {
         for (Entry<String, Pair<String, SubTileEntityInGame>> entries : RandomTweaker.subTileGeneratingMap.entrySet()) {
             if (entries.getKey().equals(name)) {
+                subTileName = name;
                 setSubTile((SubTileEntity) entries.getValue().getValue());
                 ci.cancel();
             }
         }
     }
 
-    @Inject(method = "readPacketNBT", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NBTTagCompound;getString(Ljava/lang/String;)Ljava/lang/String;", shift = Shift.AFTER), cancellable = true, remap = true)
+    @Inject(method = "readPacketNBT", at = @At(value = "HEAD"), cancellable = true)
     public void readPacketNBT(NBTTagCompound cmp, CallbackInfo ci) {
-        NBTTagCompound subCmp = cmp.getCompoundTag(TAG_SUBTILE_NAME);
+        super.readPacketNBT(cmp);
+        subTileName = cmp.getString(TAG_SUBTILE_NAME);
+
+        NBTTagCompound subCmp = cmp.getCompoundTag(TAG_SUBTILE_CMP);
 
         try {
             if (subTile == null || !BotaniaAPI.getSubTileStringMapping(subTile.getClass()).equals(subTileName))
