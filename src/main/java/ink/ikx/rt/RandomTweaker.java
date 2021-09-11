6d8c2fe37cb53fee668fd688654cffb01081a91d
superhelo <xiaohaluo@qq.com>
2021-09-11 18:42:59 +0800

    1.1.16

--- src/main/java/ink/ikx/rt/RandomTweaker.java
+++ src/main/java/ink/ikx/rt/RandomTweaker.java
@@ -58,7 +58,7 @@ public class RandomTweaker {
 
     public static final String MODID = "randomtweaker";
     public static final String NAME = "RandomTweaker";
-    public static final String VERSION = "1.1.15";
+    public static final String VERSION = "1.1.16";
     public static final String GUI_FACTORY = "ink.ikx.rt.impl.config.RTConfigGuiFactory";
     public static final String DESPENDENCIES = "required-after:crafttweaker;after:contenttweaker";
 
