684d8cd4fbca5b36dbdbfa8f6e8eb73f87df16e4
superhelo <xiaohaluo@qq.com>
2021-10-01 11:29:17 +0800

    refactor(manaBaubleEvent): move location

--- src/main/java/ink/ikx/rt/impl/mods/contenttweaker/subtile/MCSubTileEntityRegEvent.java
+++ src/main/java/ink/ikx/rt/impl/mods/contenttweaker/subtile/MCSubTileEntityRegEvent.java
@@ -1,18 +1,24 @@
 package ink.ikx.rt.impl.mods.contenttweaker.subtile;
 
+import baubles.api.BaublesApi;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.lang.Pair;
 import com.teamacronymcoders.contenttweaker.ContentTweaker;
 import ink.ikx.rt.Main;
 import ink.ikx.rt.api.internal.file.IProp;
 import ink.ikx.rt.api.mods.contenttweaker.subtile.ISubTileEntityRepresentation;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import vazkii.botania.api.BotaniaAPIClient;
-
+import ink.ikx.rt.impl.mods.contenttweaker.mana.bauble.MCManaBaubleContent;
 import java.io.File;
 import java.util.Map;
+import java.util.Objects;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.items.IItemHandler;
+import vazkii.botania.api.BotaniaAPIClient;
 
 public class MCSubTileEntityRegEvent {
 
@@ -33,13 +39,29 @@ public class MCSubTileEntityRegEvent {
             + "      }\n"
             + "    }]\n"
             + "  }\n"
-            + "}";
+        + "}";
 
     @SubscribeEvent
     public static void onModelRegistry(ModelRegistryEvent event) {
         for (Map.Entry<String, Pair<String, ISubTileEntityRepresentation>> entry : Main.SUB_TILE_GENERATING_MAP.entrySet()) {
             createFlowerBlockState(entry.getKey());
             BotaniaAPIClient.registerSubtileModel(entry.getKey(), new ModelResourceLocation(ContentTweaker.MOD_ID + ":" + entry.getKey()));
+        }
+    }
+
+    @SubscribeEvent
+    public static void onDeath(LivingDeathEvent evt) {
+        if (!evt.getEntityLiving().world.isRemote
+            && evt.getEntityLiving() instanceof EntityPlayer
+            && !evt.getEntityLiving().world.getGameRules().getBoolean("keepInventory")
+            && !((EntityPlayer) evt.getEntityLiving()).isSpectator()) {
+            IItemHandler inv = BaublesApi.getBaublesHandler((EntityPlayer) evt.getEntityLiving());
+            for (int i = 0; i < inv.getSlots(); i++) {
+                ItemStack stack = inv.getStackInSlot(i);
+                if (!stack.isEmpty() && Objects.requireNonNull(stack.getItem().getRegistryName()).getNamespace().equals("contenttweaker")) {
+                    ((MCManaBaubleContent) stack.getItem()).onUnequipped(stack, evt.getEntityLiving());
+                }
+            }
         }
     }
 
