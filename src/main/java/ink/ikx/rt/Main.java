0ac4d07caa56d746d59b6654b8a370c39b2fe5db
ikexing <ikexing@qq.com>
2021-10-22 13:15:01 +0800

    feature(all): releases 1.2.5
    fix(manabar): blue mana is not rended.
    fix(Main, IJeiSlot) change the error name and adding missing annotation

--- src/main/java/ink/ikx/rt/Main.java
+++ src/main/java/ink/ikx/rt/Main.java
@@ -47,7 +47,7 @@ public class Main {
 
     public static final String MODID = "randomtweaker";
     public static final String NAME = "RandomTweaker";
-    public static final String VERSION = "1.2.4";
+    public static final String VERSION = "1.2.5";
     public static final String DESPENDENCIES = "required-after:crafttweaker;after:contenttweaker;";
 
     public static final Set<IJeiPanel> JEI_PANEL_SET = Sets.newHashSet();
