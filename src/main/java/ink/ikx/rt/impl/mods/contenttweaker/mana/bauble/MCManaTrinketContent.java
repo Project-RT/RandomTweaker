82a3b3d1b42906c8247ce5b586e159ba619103b7
superhelo <xiaohaluo@qq.com>
2022-01-10 23:02:23 +0800

    feature(ManaTrinket): Fix not hot reload bug

--- src/main/java/ink/ikx/rt/impl/mods/contenttweaker/mana/bauble/MCManaTrinketContent.java
+++ src/main/java/ink/ikx/rt/impl/mods/contenttweaker/mana/bauble/MCManaTrinketContent.java
@@ -2,10 +2,12 @@ package ink.ikx.rt.impl.mods.contenttweaker.mana.bauble;
 
 import ink.ikx.rt.api.mods.contenttweaker.mana.bauble.IManaBaubleRepresentation;
 import vazkii.botania.api.item.ICosmeticBauble;
+import youyihj.zenutils.api.cotx.annotation.ExpandContentTweakerEntry;
 
 /**
  * @author superhelo
  */
+@ExpandContentTweakerEntry
 public class MCManaTrinketContent extends MCManaBaubleContent implements ICosmeticBauble {
 
     public MCManaTrinketContent(IManaBaubleRepresentation manaBauble) {
