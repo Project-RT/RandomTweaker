165b609839fea8d927ea7c0ee4b511da0604273a
superhelo <xiaohaluo@qq.com>
2022-01-16 23:00:35 +0800

    fix(IBlockPosExpansion): Fix "n" parameter default is 0

--- src/main/java/ink/ikx/rt/api/internal/world/IBlockPosExpansion.java
+++ src/main/java/ink/ikx/rt/api/internal/world/IBlockPosExpansion.java
@@ -2,10 +2,13 @@ package ink.ikx.rt.api.internal.world;
 
 import crafttweaker.api.minecraft.CraftTweakerMC;
 import crafttweaker.api.world.IBlockPos;
-import net.minecraft.util.math.BlockPos;
-import stanhebben.zenscript.annotations.*;
-
 import java.util.stream.StreamSupport;
+import net.minecraft.util.math.BlockPos;
+import stanhebben.zenscript.annotations.Optional;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenExpansion;
+import stanhebben.zenscript.annotations.ZenMethod;
+import stanhebben.zenscript.annotations.ZenMethodStatic;
 
 @ZenExpansion("crafttweaker.world.IBlockPos")
 @ZenClass("mods.randomtweaker.vanilla.IBlockPos")
@@ -30,32 +33,32 @@ public abstract class IBlockPosExpansion {
     }
 
     @ZenMethod
-    public static IBlockPos up(IBlockPos pos, @Optional int n) {
+    public static IBlockPos up(IBlockPos pos, @Optional(valueLong = 1) int n) {
         return CraftTweakerMC.getIBlockPos(CraftTweakerMC.getBlockPos(pos).up(n));
     }
 
     @ZenMethod
-    public static IBlockPos down(IBlockPos pos, @Optional int n) {
-        return CraftTweakerMC.getIBlockPos(CraftTweakerMC.getBlockPos(pos).down());
+    public static IBlockPos down(IBlockPos pos, @Optional(valueLong = 1) int n) {
+        return CraftTweakerMC.getIBlockPos(CraftTweakerMC.getBlockPos(pos).down(n));
     }
 
     @ZenMethod
-    public static IBlockPos north(IBlockPos pos, @Optional int n) {
+    public static IBlockPos north(IBlockPos pos, @Optional(valueLong = 1) int n) {
         return CraftTweakerMC.getIBlockPos(CraftTweakerMC.getBlockPos(pos).north(n));
     }
 
     @ZenMethod
-    public static IBlockPos south(IBlockPos pos, @Optional int n) {
+    public static IBlockPos south(IBlockPos pos, @Optional(valueLong = 1) int n) {
         return CraftTweakerMC.getIBlockPos(CraftTweakerMC.getBlockPos(pos).south(n));
     }
 
     @ZenMethod
-    public static IBlockPos west(IBlockPos pos, @Optional int n) {
+    public static IBlockPos west(IBlockPos pos, @Optional(valueLong = 1) int n) {
         return CraftTweakerMC.getIBlockPos(CraftTweakerMC.getBlockPos(pos).west(n));
     }
 
     @ZenMethod
-    public static IBlockPos east(IBlockPos pos, @Optional int n) {
+    public static IBlockPos east(IBlockPos pos, @Optional(valueLong = 1) int n) {
         return CraftTweakerMC.getIBlockPos(CraftTweakerMC.getBlockPos(pos).east(n));
     }
 
