f85b9f4b97fe6a37aeec92c26c8632f1dab5e88c
ikexing <ikexing@qq.com>
2022-01-23 19:34:46 +0800

    feature(MixinFTBUltimineClient): abstract class and remove redundant imports

--- src/main/java/ink/ikx/rt/mixins/mods/ftbultimine/MixinFTBUltimineClient.java
+++ src/main/java/ink/ikx/rt/mixins/mods/ftbultimine/MixinFTBUltimineClient.java
@@ -1,20 +1,15 @@
 package ink.ikx.rt.mixins.mods.ftbultimine;
 
 import com.feed_the_beast.mods.ftbultimine.client.FTBUltimineClient;
-import ink.ikx.rt.impl.internal.capability.CapabilityRegistryHandler;
-import ink.ikx.rt.impl.internal.capability.CapabilityRegistryHandler.FTBUltimineTag;
 import ink.ikx.rt.impl.internal.utils.InternalUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Objects;
-
 @Mixin(value = FTBUltimineClient.class, remap = false)
-public class MixinFTBUltimineClient {
+public abstract class MixinFTBUltimineClient {
 
     @Inject(method = "info", at = @At("HEAD"), cancellable = true)
     public void injectInfo(RenderGameOverlayEvent.Text event, CallbackInfo ci) {
