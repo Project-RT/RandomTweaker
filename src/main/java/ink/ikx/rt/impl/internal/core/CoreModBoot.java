78ac370e13a63bad89a64ed13815ebfc18d8e5be
ikexing <ikexing@qq.com>
2022-01-23 23:34:56 +0800

    fix(mixin): Remove HackFixServer and Fix MixinLoader

--- src/main/java/ink/ikx/rt/impl/internal/core/CoreModBoot.java
+++ src/main/java/ink/ikx/rt/impl/internal/core/CoreModBoot.java
@@ -1,46 +1,24 @@
 package ink.ikx.rt.impl.internal.core;
 
-import net.minecraftforge.fml.relauncher.CoreModManager;
+import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
-import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.security.CodeSource;
 import java.util.Map;
 
 /**
  * @author ikexing
  */
 @IFMLLoadingPlugin.Name("RandomTweaker Mixin Boot")
-@IFMLLoadingPlugin.SortingIndex(-7500)
-@IFMLLoadingPlugin.MCVersion("1.12.2")
+@IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
 public class CoreModBoot implements IFMLLoadingPlugin {
 
     public CoreModBoot() {
         MixinBootstrap.init();
         LogManager.getLogger("RandomTweaker Mixins").info("registering core mixins...");
         Mixins.addConfiguration("mixins.randomtweaker.init.json");
-        Mixins.addConfiguration("mixins.randomtweaker.vanilla.json");
-
-        CodeSource codeSource = this.getClass().getProtectionDomain().getCodeSource();
-        if (codeSource != null) {
-            URL location = codeSource.getLocation();
-            try {
-                File file = new File(location.toURI());
-                if (file.isFile()) {
-                    CoreModManager.getReparseableCoremods().remove(file.getName());
-                }
-            } catch (URISyntaxException e) {
-                e.printStackTrace();
-            }
-        } else {
-            LogManager.getLogger().warn("No CodeSource, if this is not a development environment we might run into problems!");
-            LogManager.getLogger().warn(this.getClass().getProtectionDomain());
-        }
     }
 
     @Override
--- src/main/java/ink/ikx/rt/mixins/core/MixinLoader.java
+++ src/main/java/ink/ikx/rt/mixins/core/MixinLoader.java
@@ -1,56 +1,35 @@
 package ink.ikx.rt.mixins.core;
 
-import net.minecraft.launchwrapper.Launch;
-import net.minecraftforge.fml.common.*;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.ModClassLoader;
+import net.minecraftforge.fml.common.ModContainer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.transformer.MixinProcessor;
-import org.spongepowered.asm.mixin.transformer.Proxy;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.util.List;
 
-@Mixin(value = LoadController.class, remap = false)
+@Mixin(value = Loader.class, remap = false)
 public abstract class MixinLoader {
 
-    @Shadow
-    private Loader loader;
+    @Shadow private List<ModContainer> mods;
+    @Shadow private ModClassLoader modClassLoader;
 
-    @Inject(method = "distributeStateMessage(Lnet/minecraftforge/fml/common/LoaderState;[Ljava/lang/Object;)V", at = @At("HEAD"))
-    private void beforeConstructing(LoaderState state, Object[] eventData, CallbackInfo ci) throws Throwable {
-        if (state == LoaderState.CONSTRUCTING) {
+    @Inject(method = "loadMods", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/fml/common/LoadController;transition(Lnet/minecraftforge/fml/common/LoaderState;Z)V", ordinal = 1), remap = false)
+    private void initMixins(List<String> injectedModContainers, CallbackInfo ci) {
 
-            ModClassLoader modClassLoader = (ModClassLoader) eventData[0];
-
-            Mixins.addConfiguration("mixins.randomtweaker.mods.json");
-
-            for (ModContainer container : this.loader.getActiveModList()) {
-                modClassLoader.addFile(container.getSource());
+        for (ModContainer mod : mods) {
+            try {
+                modClassLoader.addFile(mod.getSource());
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
             }
-
-            Field transformerField = Proxy.class.getDeclaredField("transformer");
-            transformerField.setAccessible(true);
-            Object transformer = transformerField.get(Launch.classLoader.getTransformers().stream().filter(t -> t instanceof Proxy).findFirst().orElse(null));
-
-            Field processorField = Class.forName("org.spongepowered.asm.mixin.transformer.MixinTransformer").getDeclaredField("processor");
-            processorField.setAccessible(true);
-            Object processor = processorField.get(transformer);
-
-            Method selectConfigsMethod = MixinProcessor.class.getDeclaredMethod("selectConfigs", MixinEnvironment.class);
-            selectConfigsMethod.setAccessible(true);
-
-            MixinEnvironment env = MixinEnvironment.getCurrentEnvironment();
-            selectConfigsMethod.invoke(processor, env);
-
-            Method prepareConfigsMethod = MixinProcessor.class.getDeclaredMethod("prepareConfigs", MixinEnvironment.class);
-            prepareConfigsMethod.setAccessible(true);
-            prepareConfigsMethod.invoke(processor, env);
         }
-    }
 
+        Mixins.addConfiguration("mixins.randomtweaker.mods.json");
+    }
 }
