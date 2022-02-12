5e077f741cfeb207e059afbc7a6bf0f6b18b75ba
ikexing <ikexing@qq.com>
2022-02-12 18:51:02 +0800

    feature(FTBU) : Add method to setting FTBU reminder message

--- src/main/java/ink/ikx/rt/api/mods/ftbultimine/IPlayerExpansionFTBU.java
+++ src/main/java/ink/ikx/rt/api/mods/ftbultimine/IPlayerExpansionFTBU.java
@@ -1,6 +1,7 @@
 package ink.ikx.rt.api.mods.ftbultimine;
 
 import crafttweaker.CraftTweakerAPI;
+import crafttweaker.IAction;
 import crafttweaker.annotations.ModOnly;
 import crafttweaker.api.minecraft.CraftTweakerMC;
 import crafttweaker.api.player.IPlayer;
@@ -13,6 +14,7 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenExpansion;
 import stanhebben.zenscript.annotations.ZenMethod;
+import stanhebben.zenscript.annotations.ZenMethodStatic;
 
 import java.util.Objects;
 
@@ -21,6 +23,8 @@ import java.util.Objects;
 @ZenExpansion("crafttweaker.player.IPlayer")
 @ZenClass("mods.randomtweaker.ftbultimine.IPlayer")
 public abstract class IPlayerExpansionFTBU {
+
+    public static String langKey = null;
 
     @ZenMethod
     public static void setAllowFTBUltimine(IPlayer player, boolean flag) {
@@ -44,6 +48,31 @@ public abstract class IPlayerExpansionFTBU {
         }
         FTBUltimineTag capability = mcPlayer.getCapability(CapabilityRegistryHandler.FTB_ULTIMINE_CAPABILITY, null);
         return Objects.requireNonNull(capability).isAllow();
+    }
+
+    @ZenMethodStatic
+    public static void setFTBUReminderMessage(String langKey) {
+        CraftTweakerAPI.apply(new ActionReminderMessage(langKey));
+    }
+
+    public static class ActionReminderMessage implements IAction {
+
+        private final String langKey;
+
+        public ActionReminderMessage(String langKey) {
+            this.langKey = langKey;
+        }
+
+        @Override
+        public void apply() {
+            IPlayerExpansionFTBU.langKey = langKey;
+        }
+
+        @Override
+        public String describe() {
+            return "Setting FTBUltimine reminder message to " + langKey;
+        }
+
     }
 
 }
--- src/main/java/ink/ikx/rt/mixins/ftbultimine/MixinFTBUltimine.java
+++ src/main/java/ink/ikx/rt/mixins/ftbultimine/MixinFTBUltimine.java
@@ -1,7 +1,12 @@
 package ink.ikx.rt.mixins.ftbultimine;
 
 import com.feed_the_beast.mods.ftbultimine.FTBUltimine;
+import ink.ikx.rt.api.mods.ftbultimine.IPlayerExpansionFTBU;
+import ink.ikx.rt.impl.internal.capability.CapabilityRegistryHandler;
 import ink.ikx.rt.impl.internal.utils.InternalUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.translation.I18n;
 import net.minecraftforge.event.world.BlockEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
@@ -9,13 +14,30 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Objects;
+
 @Pseudo
 @Mixin(value = FTBUltimine.class, remap = false)
 public abstract class MixinFTBUltimine {
 
-    @Inject(method = "blockBroken", at = @At(value = "HEAD"), cancellable = true)
+    @SuppressWarnings("deprecation")
+    @Inject(
+            method = "blockBroken",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/feed_the_beast/mods/ftbultimine/FTBUltimine;rayTrace(Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/util/math/RayTraceResult;",
+                    shift = At.Shift.BEFORE
+            ),
+            cancellable = true)
     public void injectBlockBroken(BlockEvent.BreakEvent event, CallbackInfo ci) {
-        InternalUtils.decouplingMethod(ci);
+        CapabilityRegistryHandler.FTBUltimineTag capability =
+                Minecraft.getMinecraft().player.getCapability(CapabilityRegistryHandler.FTB_ULTIMINE_CAPABILITY, null);
+        if (InternalUtils.isOpenFTBUltimineControl() && !Objects.requireNonNull(capability).isAllow()) {
+            if (IPlayerExpansionFTBU.langKey != null) {
+                event.getPlayer().sendMessage(new TextComponentString(I18n.translateToLocal(IPlayerExpansionFTBU.langKey)));
+            }
+            ci.cancel();
+        }
     }
 
 }
