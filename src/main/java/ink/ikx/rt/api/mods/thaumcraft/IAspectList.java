755bd7db18e2726de157ae182ae9aeb04c46ea2a
ikexing <ikexing@qq.com>
2022-04-10 20:13:59 +0800

    Revert "thaumcraft : add a method to get all stack with aspect"
    This reverts commit c73f98897a349a815896367fc316a4a057fa2b40.

--- src/main/java/ink/ikx/rt/api/mods/thaumcraft/IAspectList.java
+++ src/main/java/ink/ikx/rt/api/mods/thaumcraft/IAspectList.java
@@ -1,12 +1,8 @@
 package ink.ikx.rt.api.mods.thaumcraft;
 
-import com.google.common.collect.Maps;
 import crafttweaker.annotations.ModOnly;
-import crafttweaker.api.item.IItemStack;
-import ink.ikx.rt.Main;
 import ink.ikx.rt.impl.mods.crafttweaker.RTRegister;
 import ink.ikx.rt.impl.mods.thaumcraft.MCAspectList;
-import java.util.Map;
 import stanhebben.zenscript.annotations.*;
 import thaumcraft.api.aspects.AspectList;
 
@@ -23,13 +19,6 @@ public interface IAspectList extends Iterable<IAspect> {
     @ZenMethod
     static IAspectList of() {
         return IAspectList.of(new AspectList());
-    }
-
-    @ZenMethod
-    static Map<IItemStack, IAspectList> getAllRegStackWithAspects() {
-        Map<IItemStack, IAspectList> map = Maps.newHashMap();
-        Main.OBJECT_TAGS_FOR_RT.forEach((stack, aspectList) -> map.put(stack, of(aspectList)));
-        return map;
     }
 
     @ZenMethod
