9d2282bf518eb25b9752d1737d43ce0db9302d2b
youyihj <youyi580@qq.com>
2023-08-16 22:57:15 +0800

    release 1.3.8

--- src/main/java/ink/ikx/rt/Main.java
+++ src/main/java/ink/ikx/rt/Main.java
@@ -62,7 +62,7 @@ public class Main {
 
     public static final String MODID = "randomtweaker";
     public static final String NAME = "RandomTweaker";
-    public static final String VERSION = "1.3.7";
+    public static final String VERSION = "1.3.8";
     public static final String DESPENDENCIES = "required-after:crafttweaker;required-after:mixinbooter@[4.2,);after:contenttweaker;after:thaumcraft";
 
     public static final Set<IJeiPanel> JEI_PANEL_SET = Sets.newHashSet();
