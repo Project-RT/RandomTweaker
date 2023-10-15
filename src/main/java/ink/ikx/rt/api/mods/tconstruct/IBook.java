9dc3d4ea7cbc73d05aed2d1001ed8f523a14a3be
ukmojb <105061290+ukmojb@users.noreply.github.com>
2023-10-15 11:51:06 +0800

    111

--- src/main/java/ink/ikx/rt/api/mods/tconstruct/IBook.java
+++ src/main/java/ink/ikx/rt/api/mods/tconstruct/IBook.java
@@ -1,8 +1,10 @@
-package ink.ikx.rt.api.mods.tinkers;
+package ink.ikx.rt.api.mods.tconstruct;
 
 import crafttweaker.annotations.ModOnly;
 import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
 import ink.ikx.rt.impl.mods.crafttweaker.RTRegister;
+import net.minecraft.item.ItemStack;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
@@ -23,7 +25,7 @@ public abstract class IBook {
 
     public static List<String> hiddenmaterialList = new ArrayList<>();
 
-    public static HashMap<String, IItemStack> materialshowitem = new HashMap<String, IItemStack>();
+    public static HashMap<String, ItemStack> materialshowitem = new HashMap<String, ItemStack>();
 
     @ZenMethod
     public static void addHiddenMaterial(String material) {
@@ -32,6 +34,6 @@ public abstract class IBook {
 
     @ZenMethod
     public static void changeMaterialItem(String material, IItemStack item) {
-        materialshowitem.put(material, item);
+        materialshowitem.put(material, CraftTweakerMC.getItemStack(item));
     }
 }
--- src/main/java/ink/ikx/rt/mixins/tconstruct/MixinAbstractMaterialSectionTransformer.java
+++ src/main/java/ink/ikx/rt/mixins/tconstruct/MixinAbstractMaterialSectionTransformer.java
@@ -1,9 +1,8 @@
-package ink.ikx.rt.mixins.tinkers;
+package ink.ikx.rt.mixins.tconstruct;
 
 
 import crafttweaker.api.minecraft.CraftTweakerMC;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,6 +16,7 @@ import slimeknights.mantle.client.gui.book.element.ElementImage;
 import slimeknights.mantle.client.gui.book.element.ElementItem;
 import slimeknights.mantle.client.gui.book.element.SizedBookElement;
 import slimeknights.tconstruct.library.TinkerRegistry;
+import slimeknights.tconstruct.library.book.content.ContentMaterial;
 import slimeknights.tconstruct.library.book.content.ContentPageIconList;
 import slimeknights.tconstruct.library.book.sectiontransformer.AbstractMaterialSectionTransformer;
 import slimeknights.tconstruct.library.book.sectiontransformer.SectionTransformer;
@@ -29,15 +29,10 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.stream.Collectors;
 
-import static ink.ikx.rt.api.mods.tinkers.IBook.hiddenmaterialList;
-import static ink.ikx.rt.api.mods.tinkers.IBook.materialshowitem;
+import static ink.ikx.rt.api.mods.tconstruct.IBook.hiddenmaterialList;
+import static ink.ikx.rt.api.mods.tconstruct.IBook.materialshowitem;
 
-/**
- * Created by IntelliJ IDEA.
- *
- * @Author : wdcftgg
- * @create 2023/10/3 18:58
- */
+@Pseudo
 @Mixin(value = AbstractMaterialSectionTransformer.class, remap = false)
 public abstract class MixinAbstractMaterialSectionTransformer extends SectionTransformer {
 
@@ -64,28 +59,28 @@ public abstract class MixinAbstractMaterialSectionTransformer extends SectionTra
         List<Material> materialList = TinkerRegistry.getAllMaterials().stream().filter((m) -> !m.isHidden()).filter(Material::hasItems).filter(this::isValidMaterial).collect(Collectors.toList());
 
 
+
         if (!materialList.isEmpty()) {
             List<ContentPageIconList> listPages = ContentPageIconList.getPagesNeededForItemCount(materialList.size(), data, book.translate(this.sectionName));
             ListIterator<ContentPageIconList> iter = listPages.listIterator();
             ContentPageIconList overview = (ContentPageIconList)iter.next();
-            Iterator var7 = materialList.iterator();
 
-            while(var7.hasNext()) {
-                Material material = (Material)var7.next();
+            for(Material material : materialList) {
                 if (!hiddenmaterialList.contains(material.getIdentifier())) {
-                    PageData page = this.addPage(data, material.getIdentifier(), "toolmaterial", this.getPageContent(material));
-                    Object icon;
+                    PageData page = addPage(data, material.getIdentifier(), ContentMaterial.ID, getPageContent(material));
+
+                    SizedBookElement icon;
                     if (material.getRepresentativeItem() != null) {
-                        icon = new ElementItem(0, 0, 1.0F, material.getRepresentativeItem());
+                        icon = new ElementItem(0, 0, 1f, material.getRepresentativeItem());
                     } else {
                         icon = new ElementImage(ImageData.MISSING);
                     }
 
                     if (materialshowitem.containsKey(material.getIdentifier()))
-                        icon = new ElementItem(0, 0, 1.0F, CraftTweakerMC.getItemStack(materialshowitem.get(material.getIdentifier())));
+                        icon = new ElementItem(0, 0, 1.0F, materialshowitem.get(material.getIdentifier()));
 
-                    while (!overview.addLink((SizedBookElement) icon, material.getLocalizedNameColored(), page)) {
-                        overview = (ContentPageIconList) iter.next();
+                    while (!overview.addLink(icon, material.getLocalizedNameColored(), page)) {
+                        overview = iter.next();
                     }
                 }
             }
