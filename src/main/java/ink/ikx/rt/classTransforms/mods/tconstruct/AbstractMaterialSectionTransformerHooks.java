5fd73f247d793f2842fabd9beca553c73599d8b4
ikexing-cn <ikexing@qq.com>
2023-10-16 16:18:53 +0800

    fix: import path

--- src/main/java/ink/ikx/rt/classTransforms/mods/tconstruct/AbstractMaterialSectionTransformerHooks.java
+++ src/main/java/ink/ikx/rt/classTransforms/mods/tconstruct/AbstractMaterialSectionTransformerHooks.java
@@ -1,22 +1,21 @@
 package ink.ikx.rt.classTransforms.mods.tconstruct;
 
-import ink.ikx.rt.api.mods.tconstruct.IBook;
+import ink.ikx.rt.Main;
 import slimeknights.mantle.client.gui.book.element.ElementItem;
 import slimeknights.tconstruct.library.materials.Material;
 
-import static ink.ikx.rt.api.mods.tconstruct.IBook.MATERIAL_SHOW_ITEM_MAP;
 
 public class AbstractMaterialSectionTransformerHooks {
 
     public static boolean isMaterialInHiddenItems(Material material) {
-        return IBook.HIDDEN_MATERIAL_LIST.contains(material.getIdentifier());
+        return Main.HIDDEN_MATERIAL_LIST.contains(material.getIdentifier());
     }
 
     public static boolean isMaterialInShowItemMap(Material material) {
-        return IBook.MATERIAL_SHOW_ITEM_MAP.containsKey(material.getIdentifier());
+        return Main.MATERIAL_SHOW_ITEM_MAP.containsKey(material.getIdentifier());
     }
 
     public static ElementItem createElementItem(Material material) {
-        return new ElementItem(0, 0, 1.0F, MATERIAL_SHOW_ITEM_MAP.get(material.getIdentifier()));
+        return new ElementItem(0, 0, 1.0F, Main.MATERIAL_SHOW_ITEM_MAP.get(material.getIdentifier()));
     }
 }
