0866bc64c040ef5d8c82d7b4e38abf569fe6502e
ikexing-cn <ikexing.cn@gmail.com>
2024-03-23 11:26:44 +0800

    chore: clean config

--- src/main/java/ink/ikx/rt/Main.java
+++ src/main/java/ink/ikx/rt/Main.java
@@ -68,7 +68,7 @@ public class Main {
 
     public static final String MODID = "randomtweaker";
     public static final String NAME = "RandomTweaker";
-    public static final String VERSION = "1.4.1";
+    public static final String VERSION = "1.4.2";
     public static final String DESPENDENCIES = "required-after:crafttweaker;required-after:mixinbooter@[4.2,);after:contenttweaker;after:thaumcraft";
 
     public static final Set<IJeiPanel> JEI_PANEL_SET = new HashSet<>();
