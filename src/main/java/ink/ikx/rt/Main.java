bec2aed2802cd878f197e2109e9d09bb2bd0cae4
youyihj <youyi580@qq.com>
2024-03-24 14:49:19 +0800

    release 1.4.4

--- src/main/java/ink/ikx/rt/Main.java
+++ src/main/java/ink/ikx/rt/Main.java
@@ -68,10 +68,10 @@ public class Main {
 
     public static final String MODID = "randomtweaker";
     public static final String NAME = "RandomTweaker";
-    public static final String VERSION = "1.4.2";
+    public static final String VERSION = "1.4.4";
     public static final String DESPENDENCIES = "required-after:crafttweaker;" +
             "required-after:mixinbooter@[4.2,);" +
-            "required-after:zenutils@[1.7,);" +
+            "required-after:zenutils@[1.17,);" +
             "after:contenttweaker;after:thaumcraft";
 
     public static final Set<IJeiPanel> JEI_PANEL_SET = new HashSet<>();
