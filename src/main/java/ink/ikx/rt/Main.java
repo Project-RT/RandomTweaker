d6858d7be906fa8f4e3701dd701f9ca1281b5c8b
friendlyhj <youyi580@qq.com>
2025-09-12 21:08:58 +0800

    release 1.4.9

--- src/main/java/ink/ikx/rt/Main.java
+++ src/main/java/ink/ikx/rt/Main.java
@@ -68,7 +68,7 @@ public class Main {
 
     public static final String MODID = "randomtweaker";
     public static final String NAME = "RandomTweaker";
-    public static final String VERSION = "1.4.8";
+    public static final String VERSION = "1.4.9";
     public static final String DESPENDENCIES = "required-after:crafttweaker;" +
             "required-after:mixinbooter@[4.2,);" +
             "required-after:zenutils@[1.17,);" +
